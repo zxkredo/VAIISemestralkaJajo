18f625fb324f8e09f2d61a0462653af5a8200c75
zxkredo <lukasmaga3@gmail.com>
2023-12-03 09:57:41 +0100

    Runners have their own controller

--- App/Controllers/RunnerController.php
+++ App/Controllers/RunnerController.php
@@ -5,13 +5,9 @@ namespace App\Controllers;
 use App\Core\AControllerBase;
 use App\Core\Responses\Response;
 
-/**
- * Class HomeController
- * Example class of a controller
- * @package App\Controllers
- */
-class AdminController extends AControllerBase
+class RunnerController extends AControllerBase
 {
+
     /**
      * Authorize controller actions
      * @param $action
@@ -19,8 +15,7 @@ class AdminController extends AControllerBase
      */
     public function authorize($action)
     {
-        //TODO only admins will be able to log in
-        return false;
+        return $this->app->getAuth()->isLogged();
     }
 
     /**
