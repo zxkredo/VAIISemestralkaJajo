ee61d1511f0cfc17331111c859fb728c8a5944e7
zxkredo <lukasmaga3@gmail.com>
2024-01-18 15:53:50 +0100

    Helping functions for working with roles

--- App/Models/UserRole.php
+++ App/Models/UserRole.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Core\Model;
+use http\Client\Curl\User;
 
 class UserRole extends Model
 {
@@ -40,4 +41,96 @@ class UserRole extends Model
         $this->login_id = $login_id;
     }
 
+    /**
+     * If user already has the role, function returns false
+     * @param Login $login
+     * @param Role $role
+     * @return bool True if role has been added without complications
+     * @throws \Exception
+     */
+    public static function trySetRoleToUser(Login $login, Role $role): bool {
+        if (static::hasRole($login, $role))
+        {
+            return false;
+        }
+
+        $newUserRole = new UserRole();
+        $newUserRole->role_id = $role->getId();
+        $newUserRole->login_id = $login->getId();
+        $newUserRole->save();
+
+        return true;
+    }
+
+    /**
+     * Tries to remove role from user.
+     * Returns false if the user doesn't have the provided role
+     * @param Login $login
+     * @return bool True if role was successfully removed
+     */
+    public static function tryRemoveRoleFromUser(Login $login, Role $role) : bool
+    {
+        $userUserRoles = static::getAll('logins_id=?', [$login->getId()]);
+        foreach ($userUserRoles as $userUserRole)
+        {
+            if ($userUserRole->role_id == $role->getId())
+            {
+                $userUserRole->delete();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns all roles of user
+     * @param Login $login
+     * @return Role[]
+     * @throws \Exception
+     */
+    public static function getAllRolesOfUser(Login $login) : array
+    {
+        $userUserRoles = static::getAll('logins_id=?', [$login->getId()]);
+        $userRolesOutput = array();
+        foreach ($userUserRoles as $userUserRole)
+        {
+            $userRolesOutput[] = Role::getOne($userUserRole->getRoleId());
+        }
+        return $userRolesOutput;
+    }
+
+    /**
+     * Removes all connections between roles and the user
+     * @param Login $login
+     * @return void
+     * @throws \Exception
+     */
+    public static function removeAllRolesFromUser(Login $login): void
+    {
+        $userUserRoles = static::getAll('logins_id=?', [$login->getId()]);
+        foreach ($userUserRoles as $userUserRole)
+        {
+            $userUserRole->delete();
+        }
+    }
+
+    /**
+     * Returns true if user has the role
+     * @param Login $login
+     * @param Role $role
+     * @return bool
+     * @throws \Exception
+     */
+    public static function hasRole(Login $login, Role $role) : bool
+    {
+        $userRoles = static::getAllRolesOfUser($login);
+        foreach ($userRoles as $userRole)
+        {
+            if ($userRole->getId() == $role->getId())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
