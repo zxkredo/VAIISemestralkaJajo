7f0f33382b905a6a8b878b2c7f874846643bfcc9
zxkredo <lukasmaga3@gmail.com>
2024-01-20 14:41:15 +0100

    checks on clicking submit button

--- public/js/nastavenia.js
+++ public/js/nastavenia.js
@@ -4,12 +4,15 @@ function checkForm() {
     const password = document.getElementById("password");
     const passwordRepeat = document.getElementById("passwordRepeat");
     const error = document.getElementById('error');
+    const emailError = document.getElementById('emailError');
 
     if (password.value !== passwordRepeat.value) {
         error.hidden = false;
         error.innerText = "Heslá sa nezhodujú!";
         return false;
-    } else  {
+    } else if (!emailError.hidden) {
+        return false;
+    } else {
         error.hidden = true;
         error.innerText = "";
     }
